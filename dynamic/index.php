3944c5984ee0e4dca142f451631892dc774248af
Тарас Андрейко <48314744+tarasandrejko@users.noreply.github.com>
2020-04-16 10:28:38 +0300

    Add files via upload

---
+++ dynamic/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="top-header">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_02.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века; при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+									<a class="btn btn-primary" href="#">Далее</a>	
+								</div>
+							</div>
+						</div>						
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_01.jpg" alt="">					
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>	
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века, при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+										<a class="btn btn-primary" href="#">Далее</a>
+								</div>						
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+		<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_02.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века; при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+									<a class="btn btn-primary" href="#">Далее</a>	
+								</div>
+							</div>
+						</div>						
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_01.jpg" alt="">					
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>	
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века, при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+										<a class="btn btn-primary" href="#">Далее</a>
+								</div>						
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+		<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_02.jpg" alt="">
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века; при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+									<a class="btn btn-primary" href="#">Далее</a>	
+								</div>
+							</div>
+						</div>						
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="main-iner">
+						<div class="row">
+							<div class="col-7">
+								<div class="image">
+									<img src="img/img_01.jpg" alt="">					
+								</div>
+							</div>
+							<div class="col-5">
+								<div class="iner-title">
+									<h4>Игра Sekiro</h4>	
+								</div>
+								<div class="iner-text">
+									<p>Действие игры происходит в Японии в периода Сэнгоку — в конце XVI века, при этом Sekiro сочетает в себе исторические, фэнтезийные и фольклорные элементы.</p>
+								</div>
+								<div class="iner-ancor">
+										<a class="btn btn-primary" href="#">Далее</a>
+								</div>						
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+	<footer>
+		
+	</footer>
+	
+</body>
+</html>
